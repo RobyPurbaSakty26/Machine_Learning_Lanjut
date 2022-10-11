d0c6307555ec1d4b68aa4553be4d572ee9c6aa17
RobyPurbaSakty26 <obykao26@gmail.com>
2022-10-11 15:57:19 +0700

    Menhaspus semua dokumen

--- Latihan_5/script.js
+++ Latihan_5/script.js
@@ -1,3 +1,4 @@
+// ----Kolom deklarasi variabel-----
 let input = document.querySelector('input');
 let button = document.querySelector('button');
 button.addEventListener('click', onClick);
@@ -12,11 +13,47 @@ const vocab_size = 2000;
 const padding = 'post';
 const truncating = 'post';
 
+var myVar;
+// -----------------------------------
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById('loaderlabel').style.display = 'none';
+  document.getElementById('loader').style.display = 'none';
+  document.getElementById('mainAPP').style.display = 'block';
+}
+
+function detectWebGLContext() {
+  // Create canvas element. The canvas is not added to the
+  // document itself, so it is never displayed in the
+  // browser window.
+  var canvas = document.createElement('canvas');
+  // Get WebGLRenderingContext from canvas element.
+  var gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+  // Report the result.
+  if (gl && gl instanceof WebGLRenderingContext) {
+    console.log('Congratulations! Your browser supports WebGL.');
+    init();
+  } else {
+    alert('Failed to get WebGL context. Your browser or device may not support WebGL.');
+  }
+}
+
+detectWebGLContext();
+
+// ----Kolom fungsi `getInput()`-----
+// Fungsi untuk mengambil input review
 function getInput() {
   const reviewText = document.getElementById('input');
   return reviewText.value;
 }
+// -----------------------------------
 
+// ----Kolom fungsi `padSequence()`-----
+// Fungsi untuk melakukan padding
 function padSequence(sequences, maxLen, padding = 'post', truncating = 'post', pad_value = 0) {
   return sequences.map((seq) => {
     if (seq.length > maxLen) {
@@ -42,7 +79,10 @@ function padSequence(sequences, maxLen, padding = 'post', truncating = 'post', p
     return seq;
   });
 }
+// -----------------------------------
 
+// ----Kolom fungsi `predict()`-----
+// Fungsi untuk melakukan prediksi
 function predict(inputText) {
   // Mengubah input review ke dalam bentuk token
   const sequence = inputText.map((word) => {
@@ -65,7 +105,10 @@ function predict(inputText) {
 
   return score;
 }
+// -----------------------------------
 
+// ----Kolom fungsi `onClick()`-----
+// Fungsi yang dijalankan ketika tombol "Post Review" diclick
 function onClick() {
   if (!isModelLoaded) {
     alert('Model not loaded yet');
@@ -91,16 +134,21 @@ function onClick() {
     alert('Negative Review \n' + score);
   }
 }
+// -----------------------------------
 
+// ----Kolom fungsi `init()`-----
 async function init() {
   // Memanggil model tfjs
+  // model = await tf.loadLayersModel('http://127.0.0.1:5500/tfjs_model/model.json'); // Untuk VS Code Live Server
   model = await tf.loadLayersModel('http://127.0.0.1:8887/tfjs_model/model.json');
   isModelLoaded = true;
 
   //Memanggil word_index
+  // const word_indexjson = await fetch('http://127.0.0.1:5500/word_index.json'); // Untuk VS Code Live Server
   const word_indexjson = await fetch('http://127.0.0.1:8887/word_index.json');
   word2index = await word_indexjson.json();
 
   console.log(model.summary());
-  console.log('Model & Metadata Loaded Successfully');
+  console.log('Model & Metadata Loaded Succesfully');
 }
+// -----------------------------------
